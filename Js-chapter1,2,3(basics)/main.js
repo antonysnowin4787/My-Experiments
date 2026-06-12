d277afce501a61c0df3e57213a91a97a56a78893
antonysnowin4787 <antonysnowin4787@gmail.com>
2026-06-12 22:05:58 +0530

    JS-Chapter1,2,3
    i just started JS and today i learned all the basics

---
+++ Js-chapter1,2,3(basics)/main.js
@@ -0,0 +1,12 @@
+console.log("HI Guys !!!");
+console.log(alert("WOW!"));
+
+Myvar="Mathematics";
+console.log(Myvar.charAt(3));
+console.log(Myvar.indexOf("ati"));
+console.log(Myvar.slice(5,3));
+console.log(Myvar.toLowerCase());
+console.log(Myvar.includes("at"));
+console.log(Myvar.split("e"));
+console.log(Myvar.split(" "));
+console.log(Myvar.split(""));
